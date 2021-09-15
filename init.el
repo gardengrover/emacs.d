7187a377a57e3eb23ce25e45023463c1885eee79
Brandon Hong <brandon@bh-mba.local>
2021-09-15 09:55:54 -0700

    Initial commit with basic configuration.

---
+++ init.el
@@ -0,0 +1,127 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+(global-linum-mode t)
+(setq linum-format "%2d \u2502 ")
+
+(put 'set-goal-column 'disabled nil)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(dolist (package '(use-package))
+   (unless (package-installed-p package)
+       (package-install package)))
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (python-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode)
+  )
+
+(use-package moe-theme
+  :ensure t)
+(load-theme 'moe-dark t)
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package neotree
+  :ensure t)
+
+(use-package pyvenv
+  :ensure t
+  :diminish
+  :config
+  (setq pyvenv-mode-line-indicator
+        '(pyvenv-virtual-env-name ("[venv:" pyvenv-virtual-env-name "] ")))
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (lsp))))
+  (pyvenv-mode +1))
+
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (setq company-idle-delay 0.3)
+  (setq company-show-numbers t)
+  (setq company-minimum-prefix-length 2))
+
+(use-package ace-window
+  :ensure t)
+
+(use-package magit
+  :ensure t
+  :init
+  (message "Loading Magit!")
+  :config
+  (message "Loaded Magit!")
+  :bind (("C-x g" . magit-status)
+         ("C-x C-g" . magit-status)))
+
+;; (use-package projectile
+;;   :ensure t
+;;   :init
+;;   (projectile-mode +1)
+;;   :bind (:map projectile-mode-map
+;;               ("C-c p" . projectile-command-map)))
+
+;;; visual stuff
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(set-face-attribute 'default nil :height 180)
+
+;;; ace-window
+(global-set-key (kbd "M-o") 'ace-window)
+
+;;; set neo-tree window toggle
+(global-set-key [f8] 'neotree-toggle)
+
+
+;; ;;;
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    '(yaml-mode virtualenvwrapper use-package projectile pkg-info neotree moe-theme jinja2-mode emmet-mode elpy ace-window)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(flycheck lsp-mode company pyvenv ace-window use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
